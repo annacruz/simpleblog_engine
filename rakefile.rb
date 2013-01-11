07b0be724feb0d7f0ef8e1fa2d3e645828c0432a
Anna Cruz <anna.cruz@gmail.com>
2013-01-11 17:09:53 +0000

    small refactor and generalizations

--- rakefile.rb
+++ rakefile.rb
@@ -1,15 +1,21 @@
 require 'maruku'
-require 'open3'
+require 'yaml'
+
+def get_config(file)
+  YAML.load_file(file)
+end
+
+config = get_config('config/application.yml')
 
 desc 'Generates the html files from markdown'
 task :generate do
   files = []
-  Dir.foreach('./_public') do |filename|
+  Dir.foreach(config['input']) do |filename|
     files.push(filename).reject!{|f| f.each_char.first == '.'}
   end
   files.each do |file|
-   markup = Maruku.new(File.open("./_public/#{file}").read)
-   html = File.open("./public/#{file.split('.')[0]}.html",'w')
+   markup = Maruku.new(File.open("./#{config['input']}#{file}").read)
+   html = File.open("./#{config['output']}#{file.split('.')[0]}.html",'w')
    html.write(markup.to_html_document)
   end
 end
