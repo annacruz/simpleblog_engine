f248526d8e7ed4345501763a60d489f6159439f2
Anna Cruz <anna.cruz@gmail.com>
2013-02-02 10:26:28 -0200

    Testing post listing

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
-require './app.rb'
+require './app'
 require './services'
 
 describe "GET '/'" do
-    before do
-      get "/"
-    end
+
+  let(:posts) {["First Post", "Second Post"]}
+
+  before do
+    get "/"
+  end
 
   it "returns status 200" do
     last_response.should be_ok
   end
+
+  it "should show all posts" do
+    last_response.body == "First Post"
+    last_response.body == "Second Post"
+  end
+
 end
 
